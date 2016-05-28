f429848392ef01f7a066be4992b7f57b9ae48172
mjimaz <m.aljimaz@gmail.com>
2016-05-28 10:13:31 -0700

    insert ItemsList component into App

--- client/src/views/App.js
+++ client/src/views/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import ItemsList from './ItemsList';
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      list: ['list1', 'list2', 'list3'],
+    }
+  }
   render() {
     return (
       <div>
       <div>Tasks List</div>
       <SearchBar/>
+      <ItemsList itemsList = {this.state.list}/>
       </div>
     )
   }
