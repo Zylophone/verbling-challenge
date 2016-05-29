9df56cc0dbc8c9162d34bbf89269fd4e9f1a9219
mjimaz <m.aljimaz@gmail.com>
2016-05-29 11:52:33 -0700

    hide only item body add styling

--- client/src/views/ItemsList.js
+++ client/src/views/ItemsList.js
@@ -3,7 +3,7 @@ import ItemsListEntry from './ItemsListEntry';
 
 const ItemsList = ({itemsList}) => (
   <div>
-  {itemsList.map((item, index) => <ItemsListEntry item={item} key={index}/>)}
+  {itemsList.map((item, index) => <ItemsListEntry item={item} key={index} num={index}/>)}
   </div>
 );
 
--- client/src/views/ItemsListEntry.js
+++ client/src/views/ItemsListEntry.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const ItemsListEntry = ({item}) =>
-  <ul id='listitem' style={{display:item.show ? 'block':'none'}}>
+const ItemsListEntry = ({item, num}) =>
+  <ul id='listitem' >
     <li id='listtitle'>{item.title}</li>
-    <ul><li id='listbody' className='Collapse'>{item.body}</li></ul>
+    <ul><li id='listbody' style={{display:item.show ? 'block':'none'}}>{item.body}</li></ul>
   </ul>
 
 export default ItemsListEntry
